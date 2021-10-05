d4f6808418303cd8273147f0afa528593e99ef8c
Rafael do Nascimento Pereira <rnp@25ghz.net>
2021-10-05 09:16:29 +0200

    fix scripts

--- arch/build.sh
+++ arch/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --cache-from rnprnp/fpga_dev_arch:0.3 -t rnprnp/fpga_dev_arch:0.3 .
+docker build --cache-from rnprnp/fpga_dev:0.3 -t rnprnp/fpga_dev:0.3 .
--- arch/run.sh
+++ arch/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name test_fpga_dev --rm -i -t rnprnp/fpga_dev_arch:0.3 bash
+docker run --name test_fpga_dev --rm -i -t rnprnp/fpga_dev:0.3 bash
